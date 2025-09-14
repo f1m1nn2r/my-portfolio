0e3c69713493b2e830b037b04ad596e4ef242704
김민중 <f1minn2r@naver.com>
2025-09-14 15:12:57 +0900

    feat(layout): 대략적인 구조 설정
    - App.tsx : 중첩 라우팅 Outlet 사용하여 페이지별 대략적인 구조 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import About from "./pages/About";
+import Experience from "./pages/Experience";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import Behind from "./pages/Behind";
+
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<About />} />
+            <Route path="experience" element={<Experience />} />
+            <Route path="portfolio" element={<Portfolio />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="behind" element={<Behind />} />
+          </Route>
+        </Routes>
+      </Router>
+    </>
+  );
 }
