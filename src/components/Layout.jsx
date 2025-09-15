56ec40feb540618563f14158c2a5222161d784fb
김민중 <f1minn2r@naver.com>
2025-09-15 23:58:31 +0900

    feat(layout): useLocation 훅으로 현재 페이지일 때 클래스 추가

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,16 @@
-import { Outlet } from "react-router-dom";
-import Header from "./Header";
-import Footer from "./Footer";
+import { Outlet, useLocation } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
 
 export default function Layout() {
+  const location = useLocation();
+
+  const currentPage =
+    location.pathname === '/' ? 'about' : location.pathname.split('/')[1];
+
   return (
     <>
-      <div className="component-layout">
+      <div className={`component-layout ${currentPage}`}>
         <Header />
         <div className="center-component basic-inner">
           <Outlet />
