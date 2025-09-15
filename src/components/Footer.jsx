1606e04e0754753cebce55ba5667bc51c3c6d168
김민중 <f1minn2r@naver.com>
2025-09-15 23:56:28 +0900

    refactor(footer): 이력서 링크 연결

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
   return (
@@ -7,12 +7,19 @@ export default function Footer() {
         <nav className="footer__navigation" aria-label="푸터 네비게이션">
           <ul className="footer__nav-list">
             <li className="footer__nav-item">
-              <Link to="/" className="footer__nav-link">
+              <Link
+                to="/public/resume.pdf"
+                className="footer__nav-link"
+                target="_blank"
+                rel="noopener noreferrer">
                 이력서 보러가기 <i aria-hidden="true">▶</i>
               </Link>
             </li>
             <li className="footer__nav-item">
-              <Link to="/" className="footer__nav-link">
+              <Link
+                to="/"
+                className="footer__nav-link"
+                rel="noopener noreferrer">
                 포트폴리오 PDF 보러가기 <i aria-hidden="true">▶</i>
               </Link>
             </li>
