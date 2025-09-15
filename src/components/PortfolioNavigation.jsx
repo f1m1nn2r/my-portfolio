a97d5f630d6bda61835f1958f0a18ffcc48df980
ISCREAMEDU\f1minn2r <f1minn2r@i-screamedu.co.kr>
2025-09-15 19:59:31 +0900

    feat(portfolio): 카테고리별 프로젝트 그룹화를 위한 네비게이션 데이터 구조 개선

--- src/components/PortfolioNavigation.jsx
+++ src/components/PortfolioNavigation.jsx
@@ -1,19 +1,35 @@
-import { NavLink } from "react-router-dom";
-import { projects } from "../data/projects";
+import { NavLink } from 'react-router-dom';
+import { projects } from '../data/projects';
 
 export default function PortfolioNavigation() {
   return (
-    <aside className="portfolio__nav">
+    <aside className="portfolio__nav-menu">
       <div className="portfolio__nav-inner">
-        <nav>
-          <ul>
-            <li>📱 홈페이지</li>
-            {projects.map((projectsList) => {
-              <li>
-                <NavLink to={projectsList.link}>{projectsList.label}</NavLink>
-              </li>;
-            })}
-          </ul>
+        <nav className="portfolio__nav">
+          {Object.entries(
+            projects.reduce((acc, project) => {
+              if (!acc[project.category]) acc[project.category] = [];
+              acc[project.category].push(project);
+              return acc;
+            }, {}),
+          ).map(([category, projectList]) => (
+            <div key={category}>
+              <h3 className="portfolio__nav-title" role="presentation">
+                {category}
+              </h3>
+              <ul className="portfolio__nav-list">
+                {projectList.map(project => (
+                  <li className="portfolio__nav-item" key={project.id}>
+                    <NavLink
+                      className="portfolio__nav-link"
+                      to={`/portfolio/${project.id}`}>
+                      {project.label}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </nav>
       </div>
     </aside>
