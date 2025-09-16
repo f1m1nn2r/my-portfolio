e99281aada89d66b3c07285d46ebb365c6b810b8
김민중 <f1minn2r@naver.com>
2025-09-17 00:17:29 +0900

    feat(footer): 경력기술서 링크 추가

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -19,6 +19,15 @@ export default function Footer() {
               <Link
                 to="/"
                 className="footer__nav-link"
+                target="_blank"
+                rel="noopener noreferrer">
+                경력기술서 보러가기 <i aria-hidden="true">▶</i>
+              </Link>
+            </li>
+            <li className="footer__nav-item">
+              <Link
+                to="/"
+                className="footer__nav-link"
                 rel="noopener noreferrer">
                 포트폴리오 PDF 보러가기 <i aria-hidden="true">▶</i>
               </Link>
