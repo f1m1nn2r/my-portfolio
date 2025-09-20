e93404d1016b3dce3249a76f99b5ff68870dafaa
김민중 <f1minn2r@naver.com>
2025-09-20 21:51:55 +0900

    feat(portfolio): 기타 작업물 컴포넌트 호출

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import { ProjectProvider } from '../contexts/ProjectContext';
 import PortfolioNavigation from '../components/PortfolioNavigation';
+import OtherWorks from '../components/OtherWorks';
 
 export default function Portfolio() {
   return (
@@ -12,6 +13,11 @@ export default function Portfolio() {
             <Outlet />
           </div>
         </div>
+        <section className="other__works">
+          <div className="other-works__container">
+            <OtherWorks />
+          </div>
+        </section>
       </section>
     </ProjectProvider>
   );
